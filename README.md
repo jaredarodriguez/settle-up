23785f0f05f6e1e62369b461a4cc6a48b19191c2
Jared Rodriguez <jaredarodriguez@gmail.com>
2019-08-23 09:59:23 -0500

    user stories added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # settle-up
+
+#AAU I want to be able to login into a shared account using login credentials such as auser id and password
+
+#AAU I want to be able to see who owes me money and to whom I owe
+
+#AAU I want to be able to catalog expenses with a drop down menu that displays on it typical expenses associated with being in a band: gas, food, gear, hotels, tolls, etc.
+
+#AAU I want to be able to see a breakdown of how money is being spent and earned. In order to do this I plan on using D3
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 // Load express
 const express = require("express");
+const path = require("path");
 
 // Create our express app
 const app = express();
 
-// Define a "root" route directly on app
+// configure the app (app.set)
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 // Tomorrow, we'll use best practice routing
 app.get("/", function(req, res) {
   res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/home", function(req, res) {
+  res.render("home", { cohort: "sei-cc-4" });
 });
 
 // Tell the app to listen on port 3000
