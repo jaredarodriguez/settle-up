04ccaba927d481519bba5e4f6e2e6a49c487c300
Jared Rodriguez <jaredarodriguez@gmail.com>
2019-08-23 17:10:35 -0500

    install session middleware

--- config/database.js
+++ config/database.js
@@ -1,6 +1,10 @@
 var mongoose = require("mongoose");
 
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+
 mongoose.connect("mongodb://localhost/settle-up", {
   useNewUrlParser: true,
   useCreateIndex: true
 });
+
+module.exports = mongoose;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+require("dotenv").config();
 require("./config/database");
-
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
@@ -18,6 +19,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "Settle-up!",
+    resave: false,
+    saveUninitialized: true
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
