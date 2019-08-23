e2a43a83803d05532e27f5ea3a92b25783b149bb
Jared Rodriguez <jaredarodriguez@gmail.com>
2019-08-23 12:55:58 -0500

    mongoose installed

--- server.js
+++ server.js
@@ -1,24 +1,42 @@
-// Load express
-const express = require("express");
-const path = require("path");
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+require("./config/database");
 
-// Create our express app
-const app = express();
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
-// configure the app (app.set)
-app.set("view engine", "ejs");
+var app = express();
+
+// view engine setup
 app.set("views", path.join(__dirname, "views"));
-// Tomorrow, we'll use best practice routing
-app.get("/", function(req, res) {
-  res.send("<h1>Hello World!</h1>");
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.get("/home", function(req, res) {
-  res.render("home", { cohort: "sei-cc-4" });
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
-// Tell the app to listen on port 3000
-// for HTTP requests from clients
-app.listen(3000, function() {
-  console.log("Listening on port 3000");
-});
+module.exports = app;
